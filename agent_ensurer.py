018627a924e3960bc217942b6efc9307032f37cd
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-01-26 12:53:15 +0300

    ignore dead agents

--- agent_ensurer.py
+++ agent_ensurer.py
@@ -2,10 +2,12 @@ import os
 import time
 import logging
 import argparse
+from datetime import datetime, timedelta, timezone
 
 import requests
 
 log = logging.getLogger(__name__)
+dead_disconnected_timeout = timedelta(minutes=5)
 
 # TODO: add schedule
 
@@ -36,24 +38,25 @@ def main():
     resp.raise_for_status()
 
     while True:
-        resp = s.get(f'{args.base_url}app/rest/agents?fields=agent(id,name,connected,enabled,idleSinceTime)')
+        request_fields = 'id,name,connected,enabled,idleSinceTime,lastActivityTime'
+        resp = s.get(f'{args.base_url}app/rest/agents?fields=agent({request_fields})')
         resp.raise_for_status()
         agents = resp.json()
         idle_agents = [a for a in agents['agent'] if a['enabled'] and a.get('idleSinceTime')]
-        connected_idle_agents = [a for a in idle_agents if a['connected']]
+        alive_idle_agents = [a for a in idle_agents if is_alive(a)]
 
         resp = s.get(f'{args.base_url}app/rest/cloud/instances')
         resp.raise_for_status()
         cloud_instances = resp.json()
         scheduled_agents_count = sum(1 for i in cloud_instances['cloudInstance'] if i['state'] == 'scheduled_to_start')
 
-        log.info('Agents: %d idle (%d connected), %d pending. Target: %d',
+        log.info('Agents: %d idle (%d alive), %d pending. Target: %d',
                  len(idle_agents),
-                 len(connected_idle_agents),
+                 len(alive_idle_agents),
                  scheduled_agents_count,
                  args.min_idle_agents)
 
-        agents_to_start = args.min_idle_agents - len(idle_agents) - scheduled_agents_count
+        agents_to_start = args.min_idle_agents - len(alive_idle_agents) - scheduled_agents_count
         if agents_to_start > 0:
             log.info('Starting %d agents', agents_to_start)
             for _ in range(agents_to_start):
@@ -68,6 +71,16 @@ def main():
 
         log.debug('Sleeping %d seconds', args.watch_interval_sec)
         time.sleep(args.watch_interval_sec)
+
+
+def is_alive(agent):
+    if agent['connected']:
+        return True
+    last_activity_time = datetime.strptime(agent['lastActivityTime'], '%Y%m%dT%H%M%S%z')
+    now = datetime.now(timezone.utc)
+    disconnected_for = now - last_activity_time
+    is_dead = disconnected_for > dead_disconnected_timeout
+    return not is_dead
 
 
 if __name__ == '__main__':
